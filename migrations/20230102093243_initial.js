45f6d7db74b9d6aebb2bd0ed7d9984e7027095d4
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2023-01-02 22:37:11 +0800

    refactor: database schema from RSS specification

--- migrations/20230102093243_initial.js
+++ migrations/20230102093243_initial.js
@@ -3,11 +3,29 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable('users', (t) => {
-    t.increments('userId').primary()
-    t.string('username').unique()
-    t.string('encryptedPassword').notNullable()
-  })
+  return Promise.all([
+    knex.schema.createTable('users', (t) => {
+      t.increments('userId').primary()
+      t.string('username').unique()
+      t.string('encryptedPassword').notNullable()
+    }),
+    knex.schema.createTable('feeds', (t) => {
+      t.increments('feedId').primary()
+      t.integer('userId').index()
+      t.string('link').notNullable()
+      t.string('title').notNullable()
+    }),
+    knex.schema.createTable('items', (t) => {
+      t.increments('itemId').primary()
+      t.integer('feedId').index()
+      t.string('title')
+      t.string('link')
+      t.string('content')
+      t.datetime('pubDate')
+      t.string('author')
+      t.string('guid')
+    })
+  ])
 }
 
 /**
@@ -15,5 +33,7 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return Promise.all([knex.schema.dropTable('users')])
+  return Promise.all(
+    ['users', 'feeds', 'items'].map((t) => knex.schema.dropTable(t))
+  )
 }
--- seeds/initial.js
+++ seeds/initial.js
@@ -1,4 +1,5 @@
 const { encrypt } = require('../knex/users')
+const Parser = require('rss-parser')
 
 /**
  * @param { import("knex").Knex } knex
@@ -6,7 +7,36 @@ const { encrypt } = require('../knex/users')
  */
 exports.seed = async (knex) => {
   await knex('users').del()
-  await knex('users').insert([
+  const [userId] = await knex('users').insert([
     { username: 'user', encryptedPassword: await encrypt('password') }
   ])
+
+  const parser = new Parser()
+  const parsed = await parser.parseURL('https://www.reddit.com/.rss')
+
+  await knex('feeds').del()
+  const { link, title } = parsed
+  const [feedId] = await knex('feeds').insert([
+    {
+      userId,
+      link,
+      title
+    }
+  ])
+
+  await knex('items').del()
+  const values = []
+  for (const item of parsed.items) {
+    const { title, link, content, pubDate, author, id } = item
+    values.push({
+      feedId,
+      title,
+      link,
+      content,
+      pubDate,
+      author,
+      guid: id
+    })
+  }
+  await knex('items').insert(values)
 }
