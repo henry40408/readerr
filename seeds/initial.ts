21fd01c004b06de00ab31041ec37da472026e0f2
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2023-01-10 22:01:21 +0800

    chore: db:reset script

--- seeds/initial.ts
+++ seeds/initial.ts
@@ -8,7 +8,15 @@ export async function seed(knex: Knex) {
 
   const [{ userId }] = await repo.createUser('user', 'password')
   const userRepo = repo.createUserRepository(userId)
-  await userRepo.createFeed({
-    feedUrl: 'https://www.reddit.com/.rss'
-  })
+  await Promise.all([
+    userRepo.createFeed({
+      feedUrl: 'https://www.reddit.com/.rss'
+    }),
+    userRepo.createFeed({
+      feedUrl: 'http://www.nasa.gov/rss/dyn/breaking_news.rss'
+    }),
+    userRepo.createFeed({
+      feedUrl: 'https://github.com/miniflux/v2/releases.atom'
+    })
+  ])
 }
