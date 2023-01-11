dea2cf0d2fb8dd49b8600f9ca84930ca0996458e
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2023-01-11 21:39:12 +0800

    chore: production configuration

--- knexfile.ts
+++ knexfile.ts
@@ -1,4 +1,8 @@
 import { Knex } from 'knex'
+import { loadEnvConfig } from '@next/env'
+
+const dev = process.env.NODE_ENV !== 'production'
+const { DATABASE_URL } = loadEnvConfig('./', dev).combinedEnv
 
 const config: { [key: string]: Knex.Config } = {
   development: {
@@ -24,6 +28,20 @@ const config: { [key: string]: Knex.Config } = {
     pool: {
       min: 1,
       max: 1,
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      afterCreate: (conn, cb) => {
+        conn.run('PRAGMA foreign_keys = ON', cb)
+      }
+    }
+  },
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: DATABASE_URL || 'production.sqlite3'
+    },
+    useNullAsDefault: true,
+    pool: {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       afterCreate: (conn, cb) => {
